300c31b69c8e4124339e2f90aa56a2733ff74e84
徐雨 <59008817+xuxuxuyu@users.noreply.github.com>
2020-11-04 15:13:15 +0800

    Add files via upload

---
+++ src/components/Input.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Input() {
+
+  return (
+    <div>111111</div>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Input from './components/Input'
+
+ReactDOM.render(
+  <Input />,
+  document.getElementById('root')
+);
+
