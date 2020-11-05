8e014eb328c4f3fd08af9bf4836b1b4ac3ee479b
徐雨 <59008817+xuxuxuyu@users.noreply.github.com>
2020-11-05 10:32:32 +0800

    less loader

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,7 +5,9 @@ export default function Input() {
 
   return (
     <div className="wrapper">
+      <input type="text" className="addonBefore" />
       <input type="text" className="content" />
+      <input type="text" className="addonAfter" />
     </div>
   );
 }
