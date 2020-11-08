87f6a7446ab5257d679f824cffe95cb657224738
徐雨 <59008817+xuxuxuyu@users.noreply.github.com>
2020-11-08 15:15:32 +0800

    1

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,28 +1,35 @@
-import React from "react";
-import { SizeType } from "../constants";
-import "./styles/index.less"
+import React,{ useState } from "react";
+import "./styles/index.less";
 
 interface InputProps {
   addonBefore?: string;
   addonAfter?: string;
-  sizeType?: SizeType;
-  value?: string;
+  defaultValue?: string;
+  sizeType?: string;
   onChange?: (value: string) => void;
 }
 
 export default function Input(props: InputProps) {
-  console.log(props)
-  const { addonBefore, addonAfter } = props
+  const { addonBefore, addonAfter, defaultValue, sizeType="middle", onChange } = props
+  const [ value, setValue ] = useState(defaultValue || "")
+
+  const onValueChange = (event: any) => {
+    if(onChange) {
+      onChange(event.target.value)
+    }
+    setValue(event.target.value)
+  }
+
   return (
-    <div className="wrapper">
+    <div className={"wrapper" + " " + sizeType}>
       {addonBefore ? (
-        <input type="text" className="addonBefore" disabled placeholder={addonBefore}/>
+        <input type="text" className="addonBefore" disabled placeholder={addonBefore} />
       ) : null}
 
-      <input type="text" className="content" />
+      <input type="text" className="content" value={value} onChange={onValueChange} />
 
       {addonAfter ? (
-        <input type="text" className="addonAfter" disabled placeholder=".com"/>
+        <input type="text" className="addonAfter" disabled placeholder={addonAfter} />
       ) : null}
     </div>
   );
